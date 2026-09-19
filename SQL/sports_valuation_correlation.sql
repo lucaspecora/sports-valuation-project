52a72bf93d3714eacc74365bddc73b90ecc4211f
Lucas Pecora <331031959+lucaspecora@users.noreply.github.com>
2026-09-19 00:36:36 +0000

    Polish notebook and SQL: add explanatory comments, reformat CTEs

--- SQL/sports_valuation_correlation.sql
+++ SQL/sports_valuation_correlation.sql
@@ -1,54 +1,83 @@
-WITH city_rank as (
-SELECT team_id, city, RANK () OVER (PARTITION BY team_id ORDER BY years DESC, city ASC) as ranking
-FROM (
-	SELECT team_id, city, COUNT (*) as years
+-- Assigns a ranking value to each team/city pair based on the amount of years spent in that city  
+WITH city_rank AS (
+	SELECT 
+		team_id, 
+		city, 
+		RANK () OVER (PARTITION BY team_id ORDER BY years DESC, city ASC) AS ranking
+	FROM (
+		SELECT team_id, city, COUNT (*) AS years
+		FROM valuations
+		GROUP BY team_id, city
+	) AS year_count
+), 
+
+-- Creates one row for each team with its top-ranked city
+top_city AS (    
+	SELECT 
+		team_id, 
+		city
+	FROM city_rank
+	WHERE ranking = 1
+), 
+
+-- Creates a snapshot avg valution for each team
+team_evals AS ( 
+	SELECT 
+		team_id, 
+		AVG(evaluation) AS avg_eval
 	FROM valuations
-	GROUP BY team_id, city
-	) as year_count
-), top_city as (
-SELECT team_id, city
-FROM city_rank
-WHERE ranking = 1
-), team_evals as (
-SELECT team_id, AVG(evaluation) as avg_eval
-FROM valuations
-GROUP BY team_id
-), team_market as (
-SELECT te.team_id, 
-te.avg_eval, 
-mm.media_market_rank, 
-AVG(te.avg_eval) OVER() as total_avg_eval,
-AVG(mm.media_market_rank) OVER()as avg_media_market_rank
-FROM top_city tc
-JOIN team_evals te
-	ON tc.team_id = te.team_id
-JOIN media_markets mm 
-	ON tc.city = mm.market_name
-), deviations as (
-SELECT team_id, 
-avg_eval, 
-(avg_eval - total_avg_eval) as ae_dev,
-(avg_eval - total_avg_eval) * (avg_eval - total_avg_eval) as ae_dev_sq,
-media_market_rank,
-(media_market_rank - avg_media_market_rank) as mmr_dev,
-(media_market_rank - avg_media_market_rank) * (media_market_rank - avg_media_market_rank) as mmr_dev_sq,
-(avg_eval - total_avg_eval) * (media_market_rank - avg_media_market_rank) as ind_covariance
-FROM team_market
-), correlation as (
-SELECT AVG(ae_dev_sq) as avg_eval_var, 
-AVG(mmr_dev_sq) as avg_mmr_var,
-AVG(ind_covariance) as covariance,
-SQRT(AVG(ae_dev_sq)) as eval_st_dev,
-SQRT(AVG(mmr_dev_sq)) as mmr_st_dev,
-AVG(ind_covariance) / (SQRT(AVG(ae_dev_sq)) * SQRT(AVG(mmr_dev_sq))) as r,
-AVG(ind_covariance) / AVG(mmr_dev_sq) as slope
-FROM deviations 
+	GROUP BY team_id
+), 
+
+-- Displays the total avg evaluation and media market rank of all teams and years across each team row
+team_market AS (
+	SELECT 
+		te.team_id, 
+		te.avg_eval, 
+		mm.media_market_rank, 
+		AVG(te.avg_eval) OVER() AS total_avg_eval,
+		AVG(mm.media_market_rank) OVER()AS avg_media_market_rank
+	FROM top_city tc
+	JOIN team_evals te
+		ON tc.team_id = te.team_id
+	JOIN media_markets mm 
+		ON tc.city = mm.market_name
+), 
+
+-- Begins the deviation calculation for the correlation cooefficient 
+deviations AS (
+	SELECT 
+		team_id, 
+		avg_eval, 
+		(avg_eval - total_avg_eval) AS ae_dev,
+		(avg_eval - total_avg_eval) * (avg_eval - total_avg_eval) AS ae_dev_sq,
+		media_market_rank,
+		(media_market_rank - avg_media_market_rank) AS mmr_dev,
+		(media_market_rank - avg_media_market_rank) * (media_market_rank - avg_media_market_rank) AS mmr_dev_sq,
+		(avg_eval - total_avg_eval) * (media_market_rank - avg_media_market_rank) AS ind_covariance
+	FROM team_market
+), 
+
+-- Correlation computed manually to display the underlying statistics
+correlation AS (
+	SELECT 
+		AVG(ae_dev_sq) AS avg_eval_var, 
+		AVG(mmr_dev_sq) AS avg_mmr_var,
+		AVG(ind_covariance) AS covariance,
+		SQRT(AVG(ae_dev_sq)) AS eval_st_dev,
+		SQRT(AVG(mmr_dev_sq)) AS mmr_st_dev,
+		AVG(ind_covariance) / (SQRT(AVG(ae_dev_sq)) * SQRT(AVG(mmr_dev_sq))) AS r,
+		AVG(ind_covariance) / AVG(mmr_dev_sq) AS slope
+	FROM deviations 
 )
-SELECT team_id, 
-media_market_rank,
-avg_eval,
-avg_eval - (ae_dev - (slope * mmr_dev)) as predicted,
-ae_dev - (slope * mmr_dev) as residual
+
+-- Outer query that performs the regression calculation and displays each teams true and predicted evaluation along with any residual
+SELECT 
+	team_id, 
+	media_market_rank,
+	avg_eval,
+	avg_eval - (ae_dev - (slope * mmr_dev)) AS predicted,
+	ae_dev - (slope * mmr_dev) AS residual
 FROM deviations
 CROSS JOIN correlation
 ;
